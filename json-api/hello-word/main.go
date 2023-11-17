d0858dc9b237e961848d258d93a88067022c68ce
Oséias Costa <oseiasc2@gmail.com>
2023-11-16 23:27:19 -0300

    feat(hello-word): hello word server study go

---
+++ json-api/hello-word/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method is not suported", http.StatusNotFound)
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parseform() err: %v", err)
+	}
+
+	fmt.Fprintf(w, "Pos request sucessful")
+	name := r.FormValue("name")
+	adress := r.FormValue("adress")
+	fmt.Fprintf(w, "Name = %s", name)
+	fmt.Fprintf(w, "Adress = %s", adress)
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
